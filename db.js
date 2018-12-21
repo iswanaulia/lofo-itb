98fc485debd7d9ef02fe119cea27dd7676f372da
iswan <iswan.aulia@gmail.com>
2018-12-21 22:24:45 +0700

    from localhost to sandbox mongoDB database

--- db.js
+++ db.js
@@ -1,2 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/securing-rest-apis-with-jwt', { useNewUrlParser: true } );
+
+//for localhost
+//mongoose.connect('mongodb://127.0.0.1:27017/securing-rest-apis-with-jwt', { useNewUrlParser: true } );
+
+const mongoURL = 'mongodb://admin:admin123@ds115753.mlab.com:15753/lofo-itb' || process.env.MONGODB_URI
+mongoose.connect(mongoURL, { useNewUrlParser: true } );
