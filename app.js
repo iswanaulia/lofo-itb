bcbf3d240e9e63dff5570572fa6ab50edc233b5f
iswan <iswan.aulia@gmail.com>
2018-12-22 00:00:09 +0700

    integrate auth service

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
 var db = require('./db');
 console.log("Database connection ready");
@@ -11,11 +11,17 @@ app.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
 
+app.get('/login', function (req, res){
+  res.render('login.ejs');
+})
+
 var UserController = require(__root + 'user/UserController');
 app.use('/api/users', UserController);
 var AuthController = require(__root + 'auth/AuthController');
 app.use('/api/auth', AuthController);
 var PostController = require(__root + 'post/PostController');
 app.use('/api/posts', PostController);
+
+app.set('view engine', 'ejs')
 
 module.exports = app;
